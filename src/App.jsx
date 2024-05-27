0c752684681073b8d02048540724204460770a97
Arjit Prajapati <arjitprajapati@gmail.com>
2024-05-28 00:48:14 +0530

    sidebar&Addproject

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+//import NewProject from "./component/NewProject";
+import NoProjectSelected from "./component/NoProjectSelected";
 import ProjectSideBar from "./component/ProjectSideBar";
 
 function App() {
   return (
     <>
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
     <ProjectSideBar/>
+    <NoProjectSelected/>
     </main>
     </>
   );
